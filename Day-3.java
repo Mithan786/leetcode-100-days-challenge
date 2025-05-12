a025468b56073ecf0c442aea59bbbbfbe1a93b57
Mithan H S <165482312+Mithan786@users.noreply.github.com>
2025-05-12 06:55:21 -0900

    Create Day-3.java

---
+++ Day-3.java
@@ -0,0 +1,45 @@
+#Day-3
+# Question  - 1
+class Solution {
+    public int fib(int n) {
+        if(n==0){
+            return 0;
+        }
+        else if(n==1){
+            return 1;
+        }
+        int FirstTerm = 0;
+        int SecondTerm = 1;
+        for (int i = 1; i<=n; i++)
+        {
+            int ThirdTerm = FirstTerm + SecondTerm;
+            FirstTerm = SecondTerm;
+            SecondTerm = ThirdTerm;
+        }
+        return FirstTerm;
+    }
+}
+
+# Question - 2
+class Solution {
+    public int tribonacci(int n) {
+        if(n==0){
+            return 0;
+        }
+        else if(n==1 || n==2){
+            return 1;
+        }
+        int FirstTerm = 0;
+        int SecondTerm = 1;
+        int ThirdTerm = 1;
+
+        for (int i = 1; i <= n; i++){
+            int ForthTerm = FirstTerm + SecondTerm + ThirdTerm;
+
+            FirstTerm = SecondTerm;
+            SecondTerm = ThirdTerm;
+            ThirdTerm = ForthTerm;
+        }
+        return FirstTerm;
+    }
+}
