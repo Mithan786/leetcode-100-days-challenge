62509e854b077fe946db128d8e26aa372dbac9d7
Mithan H S <165482312+Mithan786@users.noreply.github.com>
2025-05-17 06:56:21 -0900

    Day-8.java

---
+++ Day-8.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int range = nums.length;
+        int actualSums = (range * (range + 1))/2;
+        int currentSums = 0;
+        for (int i=0; i<nums.length; i++){
+            currentSums = currentSums + nums[i];
+        }
+        int ans = actualSums - currentSums;
+        return ans;
+    }
+}
