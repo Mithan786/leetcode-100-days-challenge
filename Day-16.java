8eae2019c398222abbf69a9d50dc95c3c1894353
Mithan H S <165482312+Mithan786@users.noreply.github.com>
2025-05-25 06:51:23 -0900

    Create Day-16.java

---
+++ Day-16.java
@@ -0,0 +1,48 @@
+#Q1.
+  class Solution {
+    public int maxAscendingSum(int[] nums) {
+        int max = nums[0];
+        int currmax = nums[0];
+
+        for(int i=1; i<nums.length; i++){
+            if (nums[i-1] < nums[i]){
+                currmax = currmax + nums[i];
+            }
+            else{
+                max = Math.max(max, currmax);
+                currmax = nums[i];
+            }
+        }
+        
+        max = Math.max(max, currmax);
+        return max;
+    }
+}
+
+#Q2
+  class Solution {
+    public int findLucky(int[] arr) {
+        HashMap<Integer,Integer> HM = new HashMap<>();
+
+        for(int i=0; i<arr.length; i++){
+            int ele = arr[i];
+
+            if (HM.containsKey(ele)==true){
+                HM.put(ele,HM.get(ele)+1);
+            }
+            else{
+                HM.put(ele,1);
+            }
+        }
+
+        int ans =-1;
+
+        for(int key:HM.keySet()){
+            if(HM.get(key)==key){
+                ans = Math.max(ans,key);
+            }
+        }
+
+        return ans;
+    }
+}
