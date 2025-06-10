56abe8b7cbdf55253152439b3506ff0b2372b3e0
Mithan H S <165482312+Mithan786@users.noreply.github.com>
2025-06-10 08:24:59 -0900

    Create Day-32.java

---
+++ Day-32.java
@@ -0,0 +1,30 @@
+#Q-1: Longest Consecutive Sequence?
+  import java.util.HashSet;
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num); // O(1) insert
+        }
+
+        int maxLength = 0;
+
+        for (int num : set) {
+            // only start counting if it's the beginning of a sequence
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    currentStreak++;
+                }
+
+                maxLength = Math.max(maxLength, currentStreak);
+            }
+        }
+
+        return maxLength;
+    }
+}
