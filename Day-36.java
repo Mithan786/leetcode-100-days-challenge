1eccea13a80a806698dc0c550c1e542381f6b78b
Mithan H S <165482312+Mithan786@users.noreply.github.com>
2025-06-14 07:38:39 -0900

    Create Day-36.java

---
+++ Day-36.java
@@ -0,0 +1,21 @@
+#Q-1: Boats to Save People?
+  class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int start = 0;
+        int end = people.length-1;
+        int count = 0;
+
+        while(start <= end){
+            if(people[start] + people[end] <= limit){
+                start ++;
+                end --;
+            }
+            else{
+                end --;
+            }
+            count ++;
+        }
+        return count;
+    }
+}
